a443121a577a916e90a09cbe127a36c741632231
Russell Osborne <russell@burningpony.com>
2017-04-10 14:05:01 -0400

    Added self documenting home endpoint.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,12 @@ class WelcomeController < ApplicationController
   def index
     render json: {
       status: 404,
-      documentation: "https://github.com/tiy-dc-ror-2017-spring/cakewalkers_api/blob/master/README.md"
+      documentation: "https://github.com/tiy-dc-ror-2017-spring/cakewalkers_api/blob/master/README.md",
+      urls: {
+        bake_jobs: bake_jobs_url,
+        bake_jobs_in_oven: in_oven_bake_jobs_url,
+        products: products_url
+      }
     }
   end
 end
